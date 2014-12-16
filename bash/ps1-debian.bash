619d07fac982a5c40cacdf1a986a02553c00594a
ceving <ceving@gmail.com>
2014-12-16 10:48:24 +0100

    Adds export.

--- bash/ps1-debian.bash
+++ bash/ps1-debian.bash
@@ -1,1 +1,1 @@
-\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\e[1;31m\]\u@\h:\w\$\[\e[0m\]
+export PS1='\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\e[1;31m\]\u@\h:\w\$\[\e[0m\] '
