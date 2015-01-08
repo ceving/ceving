e48ab9b8bc87eb37fe12d2ff495d4238f56ee9e7
Sascha Ziemann <ceving@gmail.com>
2015-01-08 13:35:32 +0100

    Omits 'begin's if not necessary.

--- scheme/type.scm
+++ scheme/type.scm
@@ -4,15 +4,22 @@
 
 (define-syntax assert
   (syntax-rules ()
+		((assert predicate argument)
+		 (if (not (predicate argument))
+				 (error "Not a" 'predicate argument)))
     ((assert predicate argument ...)
      (begin
-       (if (not (predicate argument))
-           (error "Not a" 'predicate argument))
+			 (assert predicate argument)
        ...))))
 
 (define-syntax lambda*
   (syntax-rules ()
-    ((lambda* ((predicate argument) ...) body ...)
+    ((lambda* ((predicate argument)) body ...)
+     (lambda (argument)
+			 (assert predicate argument)
+			 body
+			 ...))
+		((lambda* ((predicate argument) ...) body ...)
      (lambda (argument ...)
        (begin
          (assert predicate argument)
