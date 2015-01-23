8bed2340cb5ac840c70620353d02acd3325eaae7
Sascha Ziemann <ceving@gmail.com>
2015-01-23 14:52:15 +0100

    Adds some keywords and the SLD extenstion for modules.

--- emacs/scheme.el
+++ emacs/scheme.el
@@ -2,6 +2,18 @@
 					(lambda ()
 						(setq-default indent-tabs-mode nil)))
 
-(put 'define* 'scheme-indent-function 1)
-(put 'lambda* 'scheme-indent-function 1)
+(add-to-list 'auto-mode-alist '("\\.sld\\'" . scheme-mode))
+
 (put 'call-with-port 'scheme-indent-function 1)
+
+(put 'define* 'scheme-indent-function 1)
+(font-lock-add-keywords 'scheme-mode '(("define*" . font-lock-keyword-face)))
+
+(put 'lambda* 'scheme-indent-function 1)
+(font-lock-add-keywords 'scheme-mode '(("lambda*" . font-lock-keyword-face)))
+
+(put 'when 'scheme-indent-function 1)
+(font-lock-add-keywords 'scheme-mode '(("when" . font-lock-keyword-face)))
+
+(put 'unless 'scheme-indent-function 1)
+(font-lock-add-keywords 'scheme-mode '(("unless" . font-lock-keyword-face)))
